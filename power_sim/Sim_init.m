bf2b960973c736c952e7541cad80b84f50a3f36f
Jerrold Erickson <jerrolde@uw.edu>
2020-07-17 10:05:25 -0700

    Updated power module. Added flags in matlab script to enable subsystems, and updated analyze script to run through multiple simulations

--- power_sim/Sim_init.m
+++ power_sim/Sim_init.m
@@ -16,11 +16,8 @@ addpath(genpath('soci-gnc/Test/'))
 init_params;
 
 
-
-%% Solar panels
-solar_panels = struct;
-
-cellArea = 26.62; % cm^2
+%% Clyde
+cellArea = 27; % cm^2
 Jmpp = 17.4; % mA / cm^2
 temperature = 28; % degrees C
 currentLossEOL = 0.99;
@@ -40,6 +37,31 @@ VmppX = cellsX * VmppCell; % V, +x face
 VmppY = cellsY * VmppCell; % V, -x, +y, -y faces
 VmppZ = cellsZ * VmppCell; % V, +z face
 
+%% DHV
+% cellArea = 30.18; % cm^2
+% Jmpp = 502.9 / cellArea; % mA / cm^2
+% temperature = 28; % degrees C
+% currentLossEOL = 0.99;
+% temperatureCurrentCoef = 0.24; % mA / degrees C
+% Impp = cellArea * Jmpp * currentLossEOL + (temperatureCurrentCoef * (temperature-28));
+% 
+% VmppBase = 2.409; % V
+% voltageLossEOL = 0.99;
+% temperatureVoltageCoef = -0.0062; % V / degrees C
+% VmppCell = VmppBase * voltageLossEOL + ((temperature-28) * temperatureVoltageCoef); % V
+% 
+% cellsX = 4;
+% cellsY = 4;
+% cellsZ = 2;
+% 
+% VmppX = cellsX * VmppCell; % V, +x face
+% VmppY = cellsY * VmppCell; % V, -x, +y, -y faces
+% VmppZ = cellsZ * VmppCell; % V, +z face
+
+
+%% Solar panels
+solar_panels = struct;
+
 solar_panels.maxPower = (Impp) .* [ VmppX; VmppY; VmppY; VmppY; VmppZ; 0.0 ];
 
 solar_panels.normals  = [ 1 -1  0  0  0  0
@@ -57,34 +79,33 @@ clear cellsX cellsY cellsZ VmppX VmppY VmppZ
 clear solar_panels
 
 %% test
-% simParams.sample_time_s = .1;
-% simParams.sensors.sample_time_s = 1;
-% simParams.sensors.actuators = 1;
-% simParams.sensors.magField = 1;
-
-
 test_data = struct;
-
-test_data.a = [0.6428 0.766 0.0];
 test_data.eff_3V3 = 0.85;
 test_data.eff_5V = 0.9;
 
 test_data.BAT = 200;
 test_data.EPS = 200;
-
 test_data.COM_TX = 2900;
 test_data.COM_RX = 450;
-
 test_data.RXW_Pointing = 2000; %% not used
 test_data.RXW_Idle = 660; %% not used
-
 test_data.MTQ_Idle = 0;
 test_data.MTQ_Active = 4*1080 + 595;
-
 test_data.IMG_Idle = 25;
 test_data.IMG_Active = 515;
-
 test_data.CDH_Idle = 398;
 test_data.CDH_Active = 398;
-
 test_data.SEN_AVG = 3.3 * (23 + 3*0.11 + 3*2.7);
+
+test_data.com_start = 2000;
+test_data.com_end = 2300;
+test_data.mtq_start = 2000;
+test_data.mtq_end = 3200;
+test_data.img_start = 2400;
+test_data.img_end = 2700;
+
+test_data.com_en = 0;
+test_data.mtq_en = 0;
+test_data.img_en = 0;
+test_data.fixed_attitude = 0;
+test_data.mode = 1;
--- power_sim/analyze_power_sim.m
+++ power_sim/analyze_power_sim.m
@@ -1,55 +1,97 @@
 close all; %clc;
-
 format short g
 
+%% run simulation
+tsim = 10800;
+test_data.com_en = 0;
+test_data.mtq_en = 0;
+test_data.img_en = 0;
+test_data.fixed_attitude = 0;
+test_data.mode = 1;
 
-%% find indexes for a single orbit
-%  Typically, simulation starts during an eclipse period
-%  this code trys to find the indexes of the first full orbit after that
-%  first eclipse period
-index_start = -1; % start of orbit to be analyzed
-index_eclipse = -1;
-index_end = -1; % end of orbit to be analyzed
-for i = 1 : length(p_in)
-    power = p_in(i);
-    if (power > 0 && index_start == -1)
-        index_start = i;
+for i = 1 : 7
+    if (i == 1)
+        test_data.fixed_attitude = 1
     end
-    if (index_start > -1)
-        if (power == 0  && index_eclipse == -1)
-            index_eclipse = i;
+    if (i == 2)
+        test_data.fixed_attitude = 0
+    end
+    if (i == 3)
+        test_data.mode = 0
+    end
+    if (i == 4)
+        test_data.mode = -1
+    end
+    if (i == 5)
+        test_data.mode = 1
+        test_data.com_en = 1
+    end
+    if (i == 6)
+        test_data.img_en = 1
+    end
+    if (i == 7)
+        test_data.com_en = 0
+        test_data.img_en = 0
+        test_data.mtq_en = 1
+    end
+    
+    runsim(tsim)
+end
+
+
+%% function run sim and print results
+function results = runsim(tsim)
+    
+    sim('Main_Sim', tsim)
+    
+    %% find indexes for a single orbit
+    %  Typically, simulation starts during an eclipse period
+    %  this code trys to find the indexes of the first full orbit after that
+    %  first eclipse period
+    index_start = -1; % start of orbit to be analyzed
+    index_eclipse = -1;
+    index_end = -1; % end of orbit to be analyzed
+    for i = 1 : length(p_in)
+        power = p_in(i);
+        if (power > 0 && index_start == -1)
+            index_start = i;
+        end
+        if (index_start > -1)
+            if (power == 0  && index_eclipse == -1)
+                index_eclipse = i;
+            end
+        end
+        if (index_eclipse > -1)
+            if (power > 0 && index_end == -1)
+                index_end = i-1;
+            end
         end
     end
-    if (index_eclipse > -1)
-        if (power > 0 && index_end == -1)
-            index_end = i-1;
-        end
+
+    %% find power budget
+    duration = index_end - index_start;
+
+    sum = 0;
+    for i = index_start:index_end
+        sum = sum + p_in(i);
     end
+
+    orbital_average_power = sum / duration;
+
+    sum = 0;
+    for i = index_start:index_end
+        sum = sum + p_out(i);
+    end
+
+    orbital_average_load = sum / duration;
+
+    power_budget = orbital_average_power - orbital_average_load;
+    power_margin = 100 * (power_budget / orbital_average_load);
+
+    %% results
+    sprintf('Analyzed data from t = %i to t = %i', tout(index_start), tout(index_end))
+    sprintf('orbital_average_power = %i', orbital_average_power)
+    sprintf('orbital_average_load = %i', orbital_average_load)
+    sprintf('power_budget = %i', power_budget)
+    sprintf('power_margin = %f', power_margin)
 end
-
-%% find power budget
-duration = index_end - index_start;
-
-sum = 0;
-for i = index_start:index_end
-    sum = sum + p_in(i);
-end
-
-orbital_average_power = sum / duration;
-
-sum = 0;
-for i = index_start:index_end
-    sum = sum + p_out(i);
-end
-
-orbital_average_load = sum / duration;
-
-power_budget = orbital_average_power - orbital_average_load;
-power_margin = 100 * (power_budget / orbital_average_load);
-
-%% results
-sprintf('Analyzed data from t = %i to t = %i', tout(index_start), tout(index_end))
-orbital_average_power
-orbital_average_load
-power_budget
-power_margin
