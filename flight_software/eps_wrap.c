baddd033eaf6ee119a93001ce7413fb9279d2c2e
ryandaepark <rdp9@uw.edu>
2021-02-27 14:11:52 -0800

    telemetry updated needs to be checked

--- flight_software/eps_wrap.c
+++ flight_software/eps_wrap.c
@@ -5,7 +5,7 @@
  * It should not be assumed to be functional.
  *
  * Jerrold Erickson, 6/23/2020
- *
+ * Ryan Park, 2/27/2021
  * Pin #75 GPIO_AD_B1_14	I2C1_SCL
  * Pin #74 GPIO_AD_B1_15	I2C1_SDA
  */
