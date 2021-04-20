0aed3884c5abffce92a41e09ecbed507a25a51f3
redhotel <77752097+redhotel@users.noreply.github.com>
2021-04-20 15:27:38 -0400

    Updated EPS sim 4/20/2021

---
+++ sim_init.m
@@ -0,0 +1,121 @@
+% SOC-i Simulation Initialization 
+%
+% This file is always the first thing that should be run when doing
+% anything with this repository
+
+clear; clc; close all;
+
+% ~~~~~~~~~~~~~~~~~~~~~
+% Add Paths
+% ~~~~~~~~~~~~~~~~~~~~~
+ addpath(genpath(pwd))
+ addpath(genpath('soci-gnc/Lib/'))
+ addpath(genpath('soci-gnc/Include/'))
+ addpath(genpath('soci-gnc/Test/'))
+
+% ~~~~~~~~~~~~~~~~~~~~~
+% Initialize Parameters
+% ~~~~~~~~~~~~~~~~~~~~~
+init_params; 
+
+%% Clyde
+% cellArea = 27; % cm^2
+% Jmpp = 17.4; % mA / cm^2
+% temperature = 28; % degrees C
+% currentLossEOL = 0.99;
+% temperatureCurrentCoef = 0.005 * cellArea; % mA / degrees C
+% Impp = cellArea * Jmpp * currentLossEOL + (temperatureCurrentCoef * (temperature-28));
+% 
+% VmppBase = 2.39; % V
+% voltageLossEOL = 0.99;
+% temperatureVoltageCoef = -0.0063; % V / degrees C
+% VmppCell = VmppBase * voltageLossEOL + ((temperature-28) * temperatureVoltageCoef); % V
+% 
+% cellsX = 4;
+% cellsY = 5;
+% cellsZ = 2;
+% 
+% VmppX = cellsX * VmppCell; % V, +x face
+% VmppY = cellsY * VmppCell; % V, -x, +y, -y faces
+% VmppZ = cellsZ * VmppCell; % V, +z face
+
+%% DHV
+cellAreaYZ = 30.18; % cm^2
+cellAreaX = 26.6; % cm^2
+ImppXBase = 441.5;  % mA 
+ImppYZBase = 502.9; % mA 
+temperature = 28; % degrees C
+currentLossEOL = 0.99;
+temperatureCurrentCoef = 0.24; % mA / degrees C
+ImppX = ImppXBase * currentLossEOL + (temperatureCurrentCoef * (temperature-28));
+ImppYZ = ImppYZBase * currentLossEOL + (temperatureCurrentCoef * (temperature-28));
+
+
+VmppBase = 2.409; % V
+voltageLossEOL = 0.99;
+temperatureVoltageCoef = -0.0062; % V / degrees C
+VmppCell = VmppBase * voltageLossEOL + ((temperature-28) * temperatureVoltageCoef); % V
+
+cellsX = 5;
+cellsY = 4;
+cellsZ = 2;
+
+% VmppX = cellsX * VmppCell; % V, +x face
+% VmppY = cellsY * VmppCell; % V, -x, +y, -y faces
+% VmppZ = cellsZ * VmppCell; % V, +z face
+
+PmppX = cellsX * VmppCell*ImppX*.95;% multiplied by a safety margin of .95
+PmppY = cellsY * VmppCell*ImppYZ*.95;% multiplied by a safety margin of .95
+PmppZ = cellsZ * VmppCell*ImppYZ*.8; % this cell doesn't have MPPT. Smaller factor used
+
+
+%% Solar panels
+solar_panels = struct;
+
+solar_panels.maxPower =  [ PmppX; PmppX; PmppY; PmppY; PmppZ; 0.0 ];
+
+solar_panels.normals  = [ 1 -1  0  0  0  0
+                          0  0  1 -1  0  0
+                          0  0  0  0  1 -1 ];
+
+solar_panels.num      = sum(solar_panels.maxPower>0);
+
+solar_panels.eff = 0.9;
+
+simParams.sensors.solar_panels = solar_panels;
+clear cellArea Jmpp temperature currentLossEOL temperatureCurrentCoef Impp 
+clear VmppBase voltageLossEOL temperatureVoltageCoef VmppCell
+clear cellsX cellsY cellsZ VmppX VmppY VmppZ 
+clear solar_panels
+
+%% test
+test_data = struct;
+test_data.eff_3V3 = 0.85;
+test_data.eff_5V = 0.85;
+
+test_data.BAT = 200;
+test_data.EPS = 200;
+test_data.COM_TX = 2900;
+test_data.COM_RX = 450;
+test_data.RXW_Pointing = 2000; %% not used
+test_data.RXW_Idle = 660; %% not used
+test_data.MTQ_Idle = 0;
+test_data.MTQ_Active = 4*1080 + 595;
+test_data.IMG_Idle = 25;
+test_data.IMG_Active = 515;
+test_data.CDH_Idle = 398;
+test_data.CDH_Active = 398;
+test_data.SEN_AVG = 3.3 * (23 + 3*0.11 + 3*2.7);
+
+test_data.com_start = 2000;
+test_data.com_end = 2300;
+test_data.mtq_start = 2000;
+test_data.mtq_end = 3200;
+test_data.img_start = 2400;
+test_data.img_end = 2700;
+
+test_data.com_en = 0;
+test_data.mtq_en = 1;
+test_data.img_en = 0;
+test_data.fixed_attitude = 0;
+test_data.mode = 1;
